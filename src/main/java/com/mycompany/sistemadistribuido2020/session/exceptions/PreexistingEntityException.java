e4bfb06ccc7874789ebd92b855224f713f53b2bc
FreddyGuapi <frddgpmll6@gmail.com>
2020-12-20 14:59:42 -0500

    PostgreSQL-Heroku-Maven

---
+++ src/main/java/com/mycompany/sistemadistribuido2020/session/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package com.mycompany.sistemadistribuido2020.session.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
